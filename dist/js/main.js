29101338a5262ff56f8c2bd0f47775999d3b16c6
tchartchke <chartai.kl@gmail.com>
2021-08-02 11:20:34 -0500

    nav bar structure

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,9 @@
 const navBtn = document.querySelector('.nav-btn');
-const menu = document.querySelector('.nav');
 const burger = document.querySelector('.nav-burger');
+const menu = document.querySelector('.nav');
+const menuBox = document.querySelector('.nav-menu');
+const menuItem = document.querySelectorAll('.nav-menu__item');
+
 
 navBtn.addEventListener('click', toggleMenu)
 
@@ -8,8 +11,14 @@ function toggleMenu() {
   // debugger;
   if (burger.classList.contains('open')){
     burger.classList.remove('open');
+    menu.classList.remove('open');
+    menuBox.classList.remove('open');
+    menuItem.forEach(item => item.classList.remove('open'));
   }
   else {
     burger.classList.add('open');
+    menu.classList.add('open');
+    menuBox.classList.add('open');
+    menuItem.forEach(item => item.classList.add('open'));
   }
 }
