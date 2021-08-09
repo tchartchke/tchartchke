382101cfb57a2e7b34456932af1f0b9c7a68554f
tchartchke <chartai.kl@gmail.com>
2021-08-09 15:31:37 -0500

    scrolls kinda. has bugs

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,52 +1,62 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const navBtn = document.querySelector('.nav-btn');
-  const burger = document.querySelector('.nav-burger');
-  const menu = document.querySelector('.nav');
-  const menuBox = document.querySelector('.nav-menu');
-  const menuItem = document.querySelectorAll('.nav-menu__item');
-  const links = document.querySelectorAll('.nav-menu__link');
-
-  function toggleMenu() {
-    if (burger.classList.contains('open')){
-      closeMenu()
+document.addEventListener(
+  'DOMContentLoaded', () =>
+  {
+    const navBtn = document.querySelector('.nav-btn');
+    const burger = document.querySelector('.nav-burger');
+    const menu = document.querySelector('.nav');
+    const menuBox = document.querySelector('.nav-menu');
+    const menuItem = document.querySelectorAll('.nav-menu__item');
+    const links = document.querySelectorAll('.nav-menu__link');
+  
+    function toggleMenu() {
+      if (burger.classList.contains('open')){
+        closeMenu()
+      }
+      else {
+        burger.classList.add('open');
+        menu.classList.add('open');
+        menuBox.classList.add('open');
+        menuItem.forEach(item => item.classList.add('open'));
+      }
     }
-    else {
-      burger.classList.add('open');
-      menu.classList.add('open');
-      menuBox.classList.add('open');
-      menuItem.forEach(item => item.classList.add('open'));
+  
+    function closeMenu(){
+      burger.classList.remove('open');
+      menu.classList.remove('open');
+      menuBox.classList.remove('open');
+      menuItem.forEach(item => item.classList.remove('open'));
     }
-  }
-
-  function closeMenu(){
-    burger.classList.remove('open');
-    menu.classList.remove('open');
-    menuBox.classList.remove('open');
-    menuItem.forEach(item => item.classList.remove('open'));
-  }
-
-
-  navBtn.addEventListener('click', toggleMenu);
-
-  links.forEach(link => link.addEventListener('click', (e) => {
-    e.preventDefault();
-    closeMenu();
+  
+    navBtn.addEventListener('click', toggleMenu);
     
-    const offset = 55;
-    const sec = document.querySelector(`.${e.target.name}`);
-    window.scrollTo({ top: (sec.offsetTop - offset), left: 0, behavior: 'smooth' });
+    links.forEach(link => link.addEventListener('click', (e) => {
+ 
+      closeMenu();
+      
+    }));
 
-  }));
+    let windowWidth = window.screen.width;
+    let scroller;
+    setScroller();
 
-  const thumbLinks = document.querySelectorAll('.project_thumb');
+    window.addEventListener('resize', setScroller);
 
-  thumbLinks.forEach(link => link.addEventListener('click', (e) => {
-    e.preventDefault();
+ 
 
-    const offset = 75;
-    const proj = document.getElementById(e.target.name);
-    window.scrollTo({ top: (proj.offsetTop - offset), left: 0, behavior: 'smooth' });
+    function setScroller() {
+      if (windowWidth < 820) {
+        scroller = new SweetScroll({
+          horizontal: true,
+          vertical: true,
+        }, window);
+      } else {
+        scroller = new SweetScroll({
+          horizontal: true,
+          vertical: true,
+        }, "#wrapper");
+      }
+    }
 
-  }))
-
-})
+  },
+  false,
+);
