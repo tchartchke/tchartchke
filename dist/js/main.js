592b75cf2c80f486608d8596e0ea6bdec75dac28
tchartchke <chartai.kl@gmail.com>
2021-08-10 11:30:11 -0500

    changing width back

--- dist/js/main.js
+++ dist/js/main.js
@@ -41,7 +41,7 @@ document.addEventListener(
 
     window.addEventListener('resize', setScroller); 
     function setScroller() {
-      if (windowWidth < 700) {
+      if (windowWidth < 820) {
         scroller = new SweetScroll({
           horizontal: false,
           vertical: true,
